ef5f214c68a9ae2b2653d463e51236521e6394ee
fengmengyang <fengmengyang@jd.com>
2018-08-14 20:01:05 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <!-- 整体容器 -->
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <!-- 左侧菜单栏 -->
+      <el-aside width="200px" height="700px" style="background-color: rgb(238, 241, 246)">
+        <NavMenu/>
+      </el-aside>
+      <!-- 右侧容器 -->
+      <el-container>
+        <!-- 顶部内容 -->
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+        <!-- 主体内容 -->
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="日期" width="140">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import NavMenu from './components/NavMenu.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavMenu
+  },
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    };
+    return {
+      tableData: Array(20).fill(item)
+    }
+  }
+}
+</script>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
